d919b5267ec71b449137c47ec066bff870829528
Diego Hernandez <Diego Hernandez>
2016-01-15 13:44:40 -0600

    fixed layout bug & connected the call fragment with the model layer

--- app/src/main/java/com/example/prefixa_01/odgtest1/CallFragment.java
+++ app/src/main/java/com/example/prefixa_01/odgtest1/CallFragment.java
@@ -5,6 +5,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.UUID;
 
@@ -14,9 +16,37 @@ import java.util.UUID;
 public class CallFragment extends Fragment {
     private static final String ARG_CLIENT_ID="client_id";
 
+    private Client mClient;
+    private TextView mCallNameTextView;
+    private TextView mCallCallerIDTextView;
+    private Button mButtonEnd;
+    private Button mButtonHold;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        //mCrime = new Crime();
+        //UUID crimeId = (UUID) getActivity().getIntent().getSerializableExtra(CrimeActivity.EXTRA_CRIME_ID);
+        UUID clientId = (UUID) getArguments().getSerializable(ARG_CLIENT_ID);
+        mClient = ClientLab.get(getActivity()).getClient(clientId);
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        return inflater.inflate(R.layout.fragment_call,container,false);
+        View view = inflater.inflate(R.layout.fragment_call,container,false);
+        mButtonEnd = (Button) view.findViewById(R.id.btn_end_call);
+        mButtonHold = (Button) view.findViewById(R.id.btn_hold_call);
+        mButtonHold.setText("Hold");
+        mButtonEnd.setText("End");
+        mCallCallerIDTextView = (TextView)view.findViewById(R.id.call_fragment_caller_id_text_view);
+        mCallNameTextView = (TextView)view.findViewById(R.id.call_fragment_name_text_view);
+        mCallCallerIDTextView.setText(mClient.getmClientID());
+        mCallNameTextView.setText(mClient.getmName());
+
+
+
+        return view;
     }
 
     public static CallFragment newInstance(UUID clientId){
